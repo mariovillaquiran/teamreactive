91dec1b80baa4b5ed02816ed5432f62041099062
maxkohn <kohn.max@gmail.com>
2021-11-10 13:15:52 -0500

    add error handling for airports where no results display

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,40 @@ var returnDateTime;
 var getFlightPrices = function(origin) {
     //need to clear input
     flightOptions.innerHTML = '';
+
+    
+
     //get variable for flighttracker api
     var flightAPI = 'https://cors-anywhere.herokuapp.com/https://api.travelpayouts.com/v1/prices/cheap?currency=usd&origin='+origin+'&token=d6d40c4eb3a903fde45b4f150345dc6d';
 
     fetch(flightAPI).then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
+
+    //if no results for this airport, show no results card
+    if (!data.data[Object.keys(data.data)[0]]) {
+        //create card container
+        var flightOptionCard = document.createElement('div');
+        //add card container class
+        flightOptionCard.classList.add('card', 'col-md', 'col-sm-12');
+        //create card body
+        var flightOptionCardBody = document.createElement('div');
+        //add card body class
+        flightOptionCardBody.classList.add('card-body');
+        //create title
+        var flightOptionCardTitle = document.createElement('h5')
+        //add title class
+        flightOptionCardTitle.classList.add('card-title');
+        //set title = city name
+        flightOptionCardTitle.textContent = 'Oops! There have not been any results for this Airport recently.  Please try another one.';
+        //add card body to card
+        flightOptionCard.append(flightOptionCardBody);
+        //add card title to card body
+        flightOptionCardBody.append(flightOptionCardTitle);
+        //append daily card to daily card container
+        flightOptions.append(flightOptionCard);
+    } else {
+
                 //console log return response
                 console.log(data);
                 //console log all aiport objects
@@ -213,11 +241,12 @@ var getFlightPrices = function(origin) {
 
                 }
 
-            });
+            }});
         } else {
             alert('Please Click Request Demo Button below and then the accompanying button on the next page');
         }
-    });
+    })
+  
 };
 
 var getFlightInput = function(event) {
