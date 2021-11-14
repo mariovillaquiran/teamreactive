13f279638968a7c7ab7eadb71582d74da5bf5a5b
Denva <emsley3684@gmail.com>
2021-11-14 17:05:17 -0500

    find airports and iata codes by city locations

--- JS/DE-script.js
+++ JS/DE-script.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
-    var getAirport= function() {
-        var flightApi = "http://aviation-edge.com/v2/public/autocomplete?key=be6698-7715a2&city=new york"
+    var formEl= document.querySelector("#city-loc")
+    var cityValEl = document.querySelector("#city-val")
+    
+    var getAp = function(event){
+        event.preventDefault();
+        var city = cityValEl.value
+
+        if(city) {
+            getAirport(city)
+            console.log(city);
+            cityValEl.value = ""
+        }
+        else {
+            alert("please enter a valid city name")
+        }
+    }
+    
+    var getAirport= function(cityName) {
+        var flightApi = "http://aviation-edge.com/v2/public/autocomplete?key=be6698-7715a2&city=" + cityName
 
         fetch(flightApi).then(function(response) {
             if(response.ok){
@@ -13,5 +30,5 @@ $(document).ready(function() {
     }
        
     
-    getAirport();   
+    formEl.addEventListener("submit", getAp);   
 });
