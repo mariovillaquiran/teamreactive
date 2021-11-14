9fa4730ceab71ac58af7e27f0c2456b7c5f02b1d
Denva <emsley3684@gmail.com>
2021-11-14 16:01:39 -0500

    added new api request, and new development test page

--- JS/DE-script.js
+++ JS/DE-script.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
     var getAirport= function() {
-        fetch()
+        var flightApi = "http://aviation-edge.com/v2/public/autocomplete?key=be6698-7715a2&city=new york"
+
+        fetch(flightApi).then(function(response) {
+            if(response.ok){
+                response.json().then(function(data) {
+                    console.log(data);
+                })
+            }
+        });
+    
     }
-    
-    
+       
     
     getAirport();   
 });
