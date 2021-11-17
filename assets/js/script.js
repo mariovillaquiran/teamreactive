86c94dd97b3b62e8c8bdef3e6ea08b08c79b8ab5
maxkohn <kohn.max@gmail.com>
2021-11-16 19:09:28 -0500

    added iata to location mapping with new api and logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,20 @@ var saveHistory = function(origin){
 }
 
 var createElements = function (flightData) {
+    //fetch city name from iata code
+    fetch("https://cors-anywhere.herokuapp.com/https://airport-info.p.rapidapi.com/airport?iata="+flightData.iata, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "airport-info.p.rapidapi.com",
+            "x-rapidapi-key": "44e449c370mshf4738c3a1b33643p1713bbjsnb7b879e3b526"
+        }
+    })
+    .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    //add location to the flightKey in each flight
+                    flightData["city"] = data.location;
+    
     //create card container
     var flightOptionCard = document.createElement('div');
     //add card container class
@@ -40,8 +54,8 @@ var createElements = function (flightData) {
     var flightOptionCardTitle = document.createElement('h5')
     //add title class
     flightOptionCardTitle.classList.add('card-title');
-    //set title = city name
-    flightOptionCardTitle.textContent = 'Flight to: '+ flightData.iata;
+    //set title = iata + location name
+    flightOptionCardTitle.textContent = 'Flight to: '+ flightData.iata+ ' / '+ flightData.city;
     //create text card
     var flightOptionCardText = document.createElement('div');
     //add card text class
@@ -83,6 +97,7 @@ var createElements = function (flightData) {
             flightOptionCardOL.append(flightOptionExpires);
             //append daily card to daily card container
             flightOptions.append(flightOptionCard);
+        })}});
 }
 
 
@@ -137,23 +152,25 @@ var getFlightPrices = function(origin) {
                 //loop through all key value pairs in data.data object
                 for (var key in data.data) {
                         //console log flight origin iata
-                        console.log(key);
+                        //console.log(key);
                         //console log first flight from api response
-                        console.log(data.data[key]);
+                        //console.log(data.data[key]);
                         for (var flightKey in  data.data[key]) {
                             //assign iata code to current object
                             data.data[key][flightKey]["iata"] = key;
+                            
                             //push each value to the flightSortArray
                             flightSortArray.push(data.data[key][flightKey]);
                         }
 
-                }
+                
+            }
                 //console.log(flightSortArray[0].price);
                 //sort flights array by price key/value pair in ascending order
                 flightSortArray.sort(function(a,b) {
                     return a.price - b.price;                
                 });
-                console.log(flightSortArray);
+                //console.log(flightSortArray);
                 //for each element in the flightSortArray
                 for (var i=0; i<flightSortArray.length; i++) {
                     //create elements for each flight
